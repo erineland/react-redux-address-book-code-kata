3ca701d5a09da3acdcbc8615a273bf04228a30bf
Erin Eland <erin@erineland.com>
2019-06-25 14:57:18 +0100

    Returning results from call to API. Fixing tests related

--- src/AddressBook/actions.js
+++ src/AddressBook/actions.js
@@ -1,27 +1,29 @@
 import { actions as searchActions } from "./SearchContacts";
 import { actions as contactDetailsActions } from "./ContactDetails";
+import { SEARCH_REQUEST_MIN_INTERVAL_IN_MILLIS } from "../httpApi/fakeHttpApi";
 
 export const updateSearchPhrase = newPhrase =>
   (dispatch, getState, { httpApi }) => {
     debugger;
-    //this takes an action
     dispatch(
       searchActions.updateSearchPhraseStart({ newPhrase }),
     );
     httpApi.getFirst5MatchingContacts({ namePart: newPhrase })
-      .then(({ data }) => {
-        const matchingContacts = data.map(contact => ({
-          id: contact.id,
-          value: contact.name,
-        }));
-        // TODO something is wrong here
-        dispatch(
-          searchActions.updateSearchPhraseSuccess({ matchingContacts: [] }),
-        );
-      })
-      .catch(() => {
-        // TODO something is missing here
-      });
+    .then(({ data }) => {
+      debugger;
+      const matchingContacts = data.map(contact => ({
+        id: contact.id,
+        value: contact.name,
+      }));
+      // FIXED - TODO something is wrong here
+      dispatch(
+        searchActions.updateSearchPhraseSuccess({ matchingContacts: matchingContacts}),
+      );
+    })
+    .catch((error) => {
+      // TODO something is missing here
+
+    });
   };
 
 export const selectMatchingContact = selectedMatchingContact =>
